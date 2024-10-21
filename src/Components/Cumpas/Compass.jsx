3d2a5d4b16d0b5378fc2ce270d6b4f2e92d61376
keyur090 <keyurvaghasiya5678@gmail.com>
2024-10-21 17:55:34 +0530

    update speedmeter

--- src/Components/Cumpas/Compass.jsx
+++ src/Components/Cumpas/Compass.jsx
@@ -1,44 +1,34 @@
-import React from 'react';
-import { FaArrowUp } from 'react-icons/fa';
-import { useSpring, animated } from 'react-spring';
-import './Compass.css';
+import React from "react";
+import Speedometer from "react-d3-speedometer"; // Import Speedometer from react-d3-speedometer
+import "./Compass.css";
 
 const Compass = ({ weather }) => {
-  const [direction, setDirection] = React.useState(0);
   const [localSpeed, setLocalSpeed] = React.useState(0);
 
   React.useEffect(() => {
     if (weather && weather.speed) {
       setLocalSpeed(weather.speed);
     }
-    
-    // You might want to use weather's wind direction instead of random direction
-    const updateCompass = () => {
-      const newDirection = Math.floor(Math.random() * 360);
-      setDirection(newDirection);
-    };
-
-    // Update direction every second
-    const intervalId = setInterval(updateCompass, 1000);
-
-    return () => clearInterval(intervalId);
   }, [weather]);
-
-  const props = useSpring({ transform: `rotate(${direction}deg)` });
 
   return (
     <div className="compass-container">
-      <div className="compass">
-        <animated.div
-          className="needle"
-          style={props}
-        >
-          <FaArrowUp />
-        </animated.div>
-      </div>
       <div className="speed">
-        <h2>Speed</h2>
-        <p>{localSpeed} km/h</p>
+        <Speedometer
+        height={130}
+        width={200}
+          minValue={0}
+          maxValue={10} // Adjust the max value based on expected speed
+          value={localSpeed}
+          needleColor="#000" // Color of the needle
+          startColor="#6e6e6e" // Color at the start of the speed range
+          endColor="#ff0000" // Color at the end of the speed range
+          segments={5} // Number of segments in the speedometer
+          textColor="rgba(255, 255, 255, 0.813)" // Color of the text
+          ringWidth={30} // Width of the speedometer ring
+          needleHeight={30} // Height of the needle
+          style={{ width: "100px" }} // Width of the speedometer
+        />
       </div>
     </div>
   );
